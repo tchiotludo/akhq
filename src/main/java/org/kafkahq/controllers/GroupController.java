40d4fac3e8d1d6ec95bce582d2cdc8b6afd3ad7c
Andreas Kohn <andreas.kohn@gmail.com>
2019-11-01 23:14:25 +0100

    Remove unused import

--- src/main/java/org/kafkahq/controllers/GroupController.java
+++ src/main/java/org/kafkahq/controllers/GroupController.java
@@ -26,7 +26,6 @@ import java.util.AbstractMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
