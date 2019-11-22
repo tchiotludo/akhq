11c977ad8ded8982ec81e75768de7f334affb828
pamarques <paulo.marques@polarising.com>
2019-11-22 14:32:36 +0000

    Add safeguard

--- src/main/java/org/kafkahq/repositories/TopicRepository.java
+++ src/main/java/org/kafkahq/repositories/TopicRepository.java
@@ -175,7 +175,11 @@ public class TopicRepository extends AbstractRepository {
                 Authentication auth = authentication.get();
                 if (auth.getAttributes().get("topics-filter-regexp") != null) {
                     //noinspection unchecked
-                    return Optional.of((List<String>)auth.getAttributes().get("topics-filter-regexp"));
+                    if(auth.getAttributes().get("topics-filter-regexp") instanceof List) {
+                        return Optional.of((List<String>) auth.getAttributes().get("topics-filter-regexp"));
+                    } else {
+                        return Optional.empty();
+                    }
                 }
             }
         }
