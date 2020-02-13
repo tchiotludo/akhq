f555b5f9cb7f0fad3ca6be484551ae3bede52cc0
Diogo Santos <diogo.fb.santos@gmail.com>
2020-02-13 18:37:25 +0000

    AKHQ-2MenuFrontEnd

--- client/src/components/common/sidebar.jsx
+++ client/src/components/common/sidebar.jsx
@@ -42,27 +42,35 @@ class Sidebar extends Component {
     }
   }
 
-  render() {
-    const {selectedTab, clusterId} = this.props;
-    const {
-      selectedConnect,
-      selectedCluster,
-      allClusters,
-      allConnects,
-      showClusters,
-      showConnects
-    } = this.state;
-    const tag = 'Snapshot';
+  setClustersAndConnects = () => {
+    const {allClusters, allConnects} = this.state;
     const listClusters = allClusters.map(cluster => (
-      <li key={cluster.id} className="list-group-item">
+      <li
+        key={cluster.id}
+        className="list-group-item"
+        onClick={() => this.setState({selectedCluster: cluster.id})}
+      >
         {cluster.id}
       </li>
     ));
     const listConnects = allConnects.map(connect => (
-      <li key={connect.name} className="list-group-item">
+      <li
+        key={connect.name}
+        className="list-group-item"
+        onClick={() => this.setState({selectedConnect: connect})}
+      >
         {connect.name}
       </li>
     ));
+    return {listClusters, listConnects};
+  };
+
+  render() {
+    const {selectedTab} = this.props;
+    const {selectedConnect, selectedCluster, showClusters, showConnects} = this.state;
+    const tag = 'Snapshot';
+
+    const {listConnects, listClusters} = this.setClustersAndConnects();
 
     console.log('connects', listConnects);
     return (
@@ -81,11 +89,9 @@ class Sidebar extends Component {
               <div className="version">{tag}</div>
             </div>
             <ul className="list-unstyled components">
-              {/*if that checks if the selected tab is "cluster"*/}
-              {/*<li className="${(tab == " cluster")?then(" active", "")}">*/}
               <li className={selectedTab === 'cluster' ? 'active' : ''}>
                 <Link
-                  to={`/${clusterId}/topic`}
+                  to={`/${selectedCluster}/topic`}
                   data-toggle="collapse"
                   aria-expanded={showClusters}
                   className="dropdown-toggle"
@@ -95,79 +101,42 @@ class Sidebar extends Component {
                 >
                   <i className="fa fa-fw fa fa-database" aria-hidden="true" />
                   Clusters
-                  <span className="badge badge-success">
-                    {/*${(connectId??)?then(connectId,"")}*/}
-                    {selectedCluster}
-                  </span>
+                  <span className="badge badge-success">{selectedCluster}</span>
                 </Link>
                 <ul className={`list-unstyled ${showClusters ? 'show' : 'collapse'}`} id="clusters">
                   {listClusters}
-                  {/*array*/}
-                  {/*for loop that dis*/}
-                  {/*<#list clusters as cluster> allClusters.map( cluster =>
-                 <li key={cluster._id} className="list-group-item"></li>)*/}
-                  <li>
-                    {/*if that checks if a cluster is selected */}
-                    {/*className="${(cluster == clusterId)?then("active", "")}"*/}
-                    <a href="#">{/*${cluster}*/}</a>
-                  </li>
-                  {/*</#list>*/}
                 </ul>
               </li>
-              {/*<#if roles?seq_contains("node") == true>*/}
-              {/*    <li className="${(tab == "node")?then("active", "")}">*/}
               <li className={this.selectedTab === 'node' ? 'active' : ''}>
                 <Link to={`/${selectedCluster}/node`}>
                   <i className="fa fa-fw fa-laptop" aria-hidden="true" /> Nodes
                 </Link>
               </li>
-              {/*</#if>*/}
-              {/*<#if roles?seq_contains("topic") == true>*/}
-              {/*    <li className="${(tab == "topic")?then("active", "")}">*/}
               <li className={this.selectedTab === 'topic' ? 'active' : ''}>
-                {/*<a href="${basePath}/${clusterId}/topic">
-            <i className="fa fa-fw fa-list" aria-hidden="true"></i> Topic</a>*/}
                 <Link to={`/${selectedCluster}/topic`}>
                   <i className="fa fa-fw fa-list" aria-hidden="true" /> Topics
                 </Link>
               </li>
-              {/*</#if>*/}
-              {/*<#if roles?seq_contains("topic/data") == true>*/}
-              {/*    <li className="${(tab == "tail")?then("active", "")}">*/}
               <li className={this.selectedTab === 'tail' ? 'active' : ''}>
                 <Link to={`/${selectedCluster}/tail`}>
                   <i className="fa fa-fw fa-level-down" aria-hidden="true" /> Live Tail
                 </Link>
               </li>
-              {/*</#if>*/}
-              {/*<#if roles?seq_contains("group") == true>*/}
-              {/*    <li className="${(tab == "group")?then("active", "")}">*/}
               <li className={this.selectedTab === 'group' ? 'active' : ''}>
                 <Link to={`/${selectedCluster}/group`}>
                   <i className="fa fa-fw fa-object-group" aria-hidden="true" /> Consumer Groups
                 </Link>
               </li>
-              {/*</#if>*/}
-              {/*<#if roles?seq_contains("acls") == true>*/}
-              {/*    <li className="${(tab == "acls")?then("active", "")}">*/}
               <li className={this.selectedTab === 'acls' ? 'active' : ''}>
                 <Link to={`/${selectedCluster}/acls`}>
                   <i className="fa fa-fw fa-key" aria-hidden="true" /> ACLS
                 </Link>
               </li>
-              {/*</#if>*/}
-              {/* <#if registryEnabled?? && registryEnabled == 
-          true && roles?seq_contains("registry") == true>*/}
-              {/*    <li className="${(tab == "schema")?then("active", "")}">*/}
               <li className={this.selectedTab === 'schema' ? 'active' : ''}>
                 <Link to={`/${selectedCluster}/schema`}>
                   <i className="fa fa-fw fa-cogs" aria-hidden="true" /> Schema Registry
                 </Link>
               </li>
-              {/*</#if>*/}
-              {/*<#if (connectList)??>*/}
-              {/*<#if roles?seq_contains("connect") == true && (connectList?size > 0)>*/}
-              {/*<li className="${(tab == "connect")?then("active", "")}">*/}
               <li className={this.selectedTab === 'connect' ? 'active' : ''}>
                 <Link
                   to={`/${selectedCluster}/connect`}
@@ -179,43 +148,19 @@ class Sidebar extends Component {
                   }}
                 >
                   <i className="fa fa-fw fa fa-exchange" aria-hidden="true" /> Connects
-                  <span className="badge badge-success">
-                    {/*${(connectId??)?then(connectId,"")}*/}
-                    {selectedConnect.name}
-                  </span>
+                  <span className="badge badge-success">{selectedConnect.name}</span>
                 </Link>
                 <ul className={`list-unstyled ${showConnects ? 'show' : 'collapse'}`} id="connects">
-                  {/*<#list connectList as connect>*/}
-
                   {listConnects}
-                  {/*<a href="#">*/}
-                  {/*    /!*${connect}*!/*/}
-                  {/*    connect-1*/}
-                  {/*</a>*/}
-
-                  {/*</#list>*/}
                 </ul>
               </li>
-              {/*</#if>*/}
-              {/*</#if>*/}
             </ul>
-            {/*<#if loginEnabled>*/}
             <div className="sidebar-log">
-              {/*<#if username??>*/}
-              {/*<a href="${basePath}/logout" data-turbolinks="false">*/}
-              {/*    <i className="fa fa-fw fa-sign-out" aria-hidden="true"></i>*/}
-              {/*    ${username} */}
-              {/*    (Logout)*/}
-              {/*</a>*/}
-              {/*<#else>*/}
               <Link to="/login" data-turbolinks="false">
                 <i className="fa fa-fw fa-sign-in" aria-hidden="true" />
                 Login
               </Link>
-              {/*</#if>*/}
-              {/*</a>*/}
             </div>
-            {/*</#if>*/}
           </nav>
         </TabContainer>
       </div>
