bbf6b9fb39f943bb8991e83b305467f38789c1fb
Carolina Martins <cnmss@iscte-iul.pt>
2020-02-19 14:50:17 +0000

    AKHQ-66 - Update Menu Items

--- src/main/java/org/kafkahq/rest/TopicResource.java
+++ src/main/java/org/kafkahq/rest/TopicResource.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
 
-import io.micronaut.http.annotation.Post;
+import io.micronaut.context.annotation.Value;
 import org.kafkahq.repositories.TopicRepository;
 import org.kafkahq.service.TopicService;
 import org.kafkahq.service.dto.TopicDTO;
@@ -20,29 +20,21 @@ public class TopicResource {
     private final Logger log = LoggerFactory.getLogger(TopicResource.class);
 
     private TopicService topicService;
-    private TopicRepository topicRepository;
 
     @Inject 
-    public TopicResource(TopicService topicService, TopicRepository topicRepository){
+    public TopicResource(TopicService topicService){
         this.topicService=topicService;
-        this.topicRepository=topicRepository;
     }
 
     @Get("/topicsByType")
-    public List<TopicDTO> fetchAllTopicsByType(String clusterId, String view) throws ExecutionException, InterruptedException {
+    public List<TopicDTO> fetchAllTopicsByType(String topicId, String view) throws ExecutionException, InterruptedException {
         log.debug("Fetch all topics by type");
-        return topicService.getAllTopicsByType(clusterId, view);
+        return topicService.getAllTopicsByType(topicId, view);
     }
 
     @Get("/topicsByName")
-    public List<TopicDTO> fetchAllTopicsByName(String clusterId, String view, String search) throws ExecutionException, InterruptedException {
+    public List<TopicDTO> fetchAllTopicsByName(String topicId, String view, String search) throws ExecutionException, InterruptedException {
         log.debug("Fetch all topics by name");
-        return topicService.getAllTopicsByName(clusterId, view, search);
-    }
-
-    @Post("/deleteTopic")
-    public String deleteTopic(String clusterId, String name) throws ExecutionException, InterruptedException {
-        topicRepository.delete(clusterId, name);
-        return "topic deleted";
+        return topicService.getAllTopicsByName(topicId, view, search);
     }
 }
