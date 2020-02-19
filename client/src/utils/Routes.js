9135945a3da1a1c3dd94b28cf69d024d87b22dbf
Lyudmyla Todoriko <lyudmyla.todoriko@polarising.com>
2020-02-19 11:39:30 +0000

    Finished fixing routes

--- client/src/utils/Routes.js
+++ client/src/utils/Routes.js
@@ -16,52 +16,55 @@ import ErrorPage from '../containers/ErrorPage';
 import api from '../services/api';
 import endpoints from '../services/endpoints';
 
-const Routes = ({ location, match }) => {
-  let path = location.pathname.split('/');
-  let clusterId = '';
-  api
-    .get(endpoints.uriClusters())
-    .then(res => {
-      console.log('here', res.data[0].id);
-      clusterId = res.data[0].id;
-    })
-    .catch(err => {
-      console.error('Error loading clusters.');
+class Routes extends Component {
+  state = {
+    clusterId: ''
+  };
+
+  componentDidMount() {
+    api.get(endpoints.uriClusters()).then(res => {
+      this.setState({ clusterId: res.data[0].id });
     });
-  console.log('clusterId', clusterId);
-  if (path[0] === 'error') {
-    return (
-      <Switch>
-        <Route exact path="/error" component={ErrorPage} />
-      </Switch>
-    );
   }
 
-  return (
-    <Base>
-      <Switch location={location}>
-        <Route exact path="/:clusterId/node" component={NodesList} />
-        <Route exact path="/:clusterId/node/:nodeId" component={NodeDetails} />
-        <Route exact path="/:clusterId/topic" component={TopicList} />
-        <Route exact path="/:clusterId/topic/:topicId" component={Topic} />
-        <Route exact path="/:clusterId/tail" component={Tail} />
-        <Route exact path="/:clusterId/group" component={Group} />
-        <Route exact path="/:clusterId/acls" component={Acls} />
-        <Route exact path="/:clusterId/schema" component={Schema} />
-        <Route exact path="/:clusterId/connect" component={Connect} />
-        <Route exact path="/:clusterId/connect/:connectId" component={Connect} />
-        <Redirect
-          path="/"
-          to={
-            match.params.clusterId
-              ? '/:clusterId/topic'
-              : !match.params.clusterId && clusterId
-              ? `/${clusterId}/topic`
-              : '/error'
-          }
-        />
-      </Switch>
-    </Base>
-  );
-};
+  render() {
+    const { location, match } = this.props;
+    const { clusterId } = this.state;
+    let path = location.pathname.split('/');
+    if (path[0] === 'error') {
+      return (
+        <Switch>
+          <Route exact path="/error" component={ErrorPage} />
+        </Switch>
+      );
+    }
+
+    return (
+      <Base>
+        <Switch location={location}>
+          <Route exact path="/:clusterId/node" component={NodesList} />
+          <Route exact path="/:clusterId/node/:nodeId" component={NodeDetails} />
+          <Route exact path="/:clusterId/topic" component={TopicList} />
+          <Route exact path="/:clusterId/topic/:topicId" component={Topic} />
+          <Route exact path="/:clusterId/tail" component={Tail} />
+          <Route exact path="/:clusterId/group" component={Group} />
+          <Route exact path="/:clusterId/acls" component={Acls} />
+          <Route exact path="/:clusterId/schema" component={Schema} />
+          <Route exact path="/:clusterId/connect" component={Connect} />
+          <Route exact path="/:clusterId/connect/:connectId" component={Connect} />
+          <Redirect
+            path="/"
+            to={
+              match.params.clusterId
+                ? '/:clusterId/topic'
+                : !match.params.clusterId && clusterId
+                ? `/${clusterId}/topic`
+                : '/error'
+            }
+          />
+        </Switch>
+      </Base>
+    );
+  }
+}
 export default withRouter(Routes);
