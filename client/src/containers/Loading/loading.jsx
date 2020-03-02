d67035ab24d6e8aa3221fd4b961a173989eecccc
angela.alves <angela.alves@polarising.com>
2020-03-02 11:34:12 +0000

    Done

--- client/src/containers/Loading/loading.jsx
+++ client/src/containers/Loading/loading.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import './styles.scss'
 import image from '../../images/logo.svg';
 class Loading extends Component {
   render() {
     return (
-      <h3 className="logo">
-        <img src={image} />
-        <sup>
-          <strong>HQ</strong>
-        </sup>
-      </h3>
+      <div className="loading">
+        <h3 className="logo">
+          <img src={image} />
+          <sup>
+            <strong>HQ</strong>
+          </sup>
+        </h3>
+      </div>
     );
   }
 }
