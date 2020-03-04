33e6b6ccae0903b38d869a952b6583b008cc5459
Lyudmyla Todoriko <lyudmyla.todoriko@polarising.com>
2020-03-04 17:11:26 +0000

    Finished Backend-Frontend implementation for Topic Delete

--- src/main/java/org/kafkahq/service/dto/topic/DeleteTopicDTO.java
+++ src/main/java/org/kafkahq/service/dto/topic/DeleteTopicDTO.java
@@ -1,4 +1,13 @@
 package org.kafkahq.service.dto.topic;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DeleteTopicDTO {
+    public String clusterId;
+    public String topicId;
 }
