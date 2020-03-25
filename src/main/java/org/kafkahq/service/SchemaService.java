8485d15075dec2086424d159627415cdf26c5d11
Gonçalo <goncalo.valentim@polarising.com>
2020-03-25 11:04:50 +0000

    - Fixed merge conflicts

--- src/main/java/org/kafkahq/service/SchemaService.java
+++ src/main/java/org/kafkahq/service/SchemaService.java
@@ -13,12 +13,11 @@ import org.kafkahq.modules.KafkaModule;
 import org.kafkahq.modules.AbstractKafkaWrapper;
 import org.kafkahq.modules.KafkaModule;
 import org.kafkahq.repositories.SchemaRegistryRepository;
-import org.kafkahq.service.dto.schema.SchemaDTO;
+import org.kafkahq.service.dto.SchemaRegistry.SchemaDTO;
 import org.kafkahq.service.dto.schema.SchemaVersionDTO;
 import org.kafkahq.service.dto.schema.UpdateSchemaDTO;
 import org.kafkahq.service.mapper.SchemaMapper;
 import org.kafkahq.service.dto.SchemaRegistry.CreateSchemaDTO;
-import org.kafkahq.service.dto.SchemaRegistry.SchemaDTO;
 import org.kafkahq.service.dto.SchemaRegistry.SchemaListDTO;
 import org.kafkahq.service.mapper.SchemaMapper;
 import org.kafkahq.utils.PagedList;
@@ -42,6 +41,9 @@ public class SchemaService {
     private SchemaRegistryRepository schemaRepository;
     private SchemaMapper schemaMapper;
 
+    @Value("${kafkahq.pagination.page-size}")
+    private Integer pageSize;
+
     @Inject
     public SchemaService(KafkaModule kafkaModule, SchemaMapper schemaMapper, SchemaRegistryRepository schemaRepository, AbstractKafkaWrapper kafkaWrapper, Environment environment) {
         this.kafkaModule = kafkaModule;
@@ -49,13 +51,6 @@ public class SchemaService {
         this.kafkaWrapper = kafkaWrapper;
         this.environment = environment;
         this.schemaRepository = schemaRepository;
-    }
-
-    public SchemaVersionDTO getLatestSchemaVersion(String clusterId, String subject) throws IOException, RestClientException {
-        Schema schemaLatestVersion = schemaRepository.getLatestVersion(clusterId, subject);
-        Schema.Config schemaLatestConfig = schemaRepository.getConfig(clusterId, subject);
-
-        return schemaMapper.fromSchemaToSchemaVersionDTO(Pair.of(schemaLatestVersion, schemaLatestConfig));
     }
 
     public SchemaListDTO getSchema(String clusterId, Optional<String> search, Optional<Integer> pageNumber)
@@ -67,19 +62,25 @@ public class SchemaService {
         return new SchemaListDTO(schemaRegistryList, list.pageCount());
     }
 
-    public int deleteSchema(String clusterId, String schema) throws ExecutionException, InterruptedException, IOException, RestClientException {
-        return schemaRepository.delete(clusterId, schema);
+    public SchemaVersionDTO getLatestSchemaVersion(String clusterId, String subject) throws IOException, RestClientException {
+        Schema schemaLatestVersion = schemaRepository.getLatestVersion(clusterId, subject);
+        Schema.Config schemaLatestConfig = schemaRepository.getConfig(clusterId, subject);
 
-        public Schema createSchema (CreateSchemaDTO schemaDTO) throws Exception {
+        return schemaMapper.fromSchemaToSchemaVersionDTO(Pair.of(schemaLatestVersion, schemaLatestConfig));
+    }
 
-
-            if (this.schemaRepository.exist(schemaDTO.getCluster(), schemaDTO.getSubject())) {
-                throw new Exception("Subject '" + schemaDTO.getSubject() + "' already exits");
-            }
+    public Schema createSchema(CreateSchemaDTO schemaDTO) throws Exception {
+        if (this.schemaRepository.exist(schemaDTO.getCluster(), schemaDTO.getSubject())) {
+            throw new Exception("Subject '" + schemaDTO.getSubject() + "' already exits");
+        }
 
         Schema register = this.registerSchema(schemaDTO.getCluster(), schemaDTO.getSubject(), schemaDTO.getSchema(), schemaDTO.getCompatibilityLevel());
 
         return register;
+    }
+
+    public int deleteSchema(String clusterId, String schema) throws ExecutionException, InterruptedException, IOException, RestClientException {
+        return schemaRepository.delete(clusterId, schema);
     }
 
     public void updateSchema(UpdateSchemaDTO updateSchemaDTO) throws IOException, RestClientException {
@@ -91,13 +92,13 @@ public class SchemaService {
 
         Schema register = this.schemaRepository.register(cluster, subject, avroSchema);
 
-            Schema.Config config = Schema.Config.builder()
-                    .compatibilityLevel(Schema.Config.CompatibilityLevelConfig.valueOf(
+        Schema.Config config = Schema.Config.builder()
+                .compatibilityLevel(Schema.Config.CompatibilityLevelConfig.valueOf(
                         compatibilityLevel
-                    ))
-                    .build();
+                ))
+                .build();
         this.schemaRepository.updateConfig(cluster, subject, config);
 
-            return register;
-        }
+        return register;
     }
+}
