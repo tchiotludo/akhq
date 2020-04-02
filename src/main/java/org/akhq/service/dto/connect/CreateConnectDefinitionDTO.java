dc53009a60bfdab785e3b0c6cd9331db75f13753
Gonçalo <goncalo.valentim@polarising.com>
2020-04-02 16:16:34 +0100

    - Added Create Definition endpoint - Updated Docker Compose configs

--- src/main/java/org/akhq/service/dto/connect/CreateConnectDefinitionDTO.java
+++ src/main/java/org/akhq/service/dto/connect/CreateConnectDefinitionDTO.java
@@ -1,11 +1,9 @@
 package org.akhq.service.dto.connect;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 import java.util.Map;
 
 @Data
@@ -14,6 +12,7 @@ import java.util.Map;
 public class CreateConnectDefinitionDTO {
     private String clusterId;
     private String connectId;
-    private  String name;
-    private  Map<String, String> configs;
+    private String name;
+    private Map<String, String> configs;
+    private String transformsValue;
 }
