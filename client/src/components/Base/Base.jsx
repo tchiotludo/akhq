12475682d0c7f496914ced7d89c8e7d3bc74d0f2
Gonçalo Valentim <goncalo_valentim@hotmail.com>
2020-04-13 09:50:05 +0100

    Update Base.jsx

--- client/src/components/Base/Base.jsx
+++ client/src/components/Base/Base.jsx
@@ -93,7 +93,6 @@ class Base extends Component {
       selectedTab
     } = this.state;
     this.checkToasts();
-    console.log('error', showErrorToast, errorToastMessage);
     return (
       <>
         <Loading show={loading} />
