2980ce76bf122a7d0078eaf984035612f950cfc8
angela.alves <angela.alves@polarising.com>
2020-04-30 12:59:50 +0100

    WIP:Sidenav

--- client/src/containers/Header/Header.jsx
+++ client/src/containers/Header/Header.jsx
@@ -2,42 +2,39 @@ import React from 'react';
 
 import { Link, withRouter } from 'react-router-dom';
 import { responsiveFontSizes } from '@material-ui/core';
+
 function Header({ title, children }) {
   let login = localStorage.getItem('login');
   return (
     <React.Fragment>
-      <p style={{ marginTop: '2.8%' }} />
-      {login === 'true' && (
-        <a
-          style={{ cursor: 'pointer', paddingLeft: '94%', responsiveFontSizes: '115%' }}
-          onClick={() => {
-            localStorage.setItem('login', 'false');
-            this.forceUpdate();
-          }}
-          data-turbolinks="false"
-        >
-          <i className="fa fa-fw fa-sign-in" aria-hidden="true" />
-          Logout
-        </a>
-      )}
-      {(login === 'false' || !login) && (
-        <button
-          data-turbolinks="false"
-          onClick={() => {
-            this.props.history.push({
-              pathname: '/login'
-            });
-          }}
-          className="btn btn-primary"
-          style={{ float: 'right' }}
-        >
-          {' '}
-          <i className="fa fa-fw fa-sign-in" aria-hidden="true" />
-          Login
-        </button>
-      )}
-      <div className="title">
-        <h1>{title}</h1>
+      <div
+        className="title"
+        style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+      >
+        {' '}
+        <h1>{title}</h1>{' '}
+        {login === 'true' && (
+          <a
+            style={{ float: 'right' }}
+            onClick={() => {
+              localStorage.setItem('login', 'false');
+              this.forceUpdate();
+            }}
+            data-turbolinks="false"
+          >
+            <i className="fa fa-fw fa-sign-in" aria-hidden="true" />
+            Logout
+          </a>
+        )}
+        {(login === 'false' || !login) && (
+          <Link to="/login">
+            <button data-turbolinks="false" className="btn btn-primary" style={{ float: 'right' }}>
+              {' '}
+              <i className="fa fa-fw fa-sign-in" aria-hidden="true" />
+              Login
+            </button>
+          </Link>
+        )}
         {children}
       </div>
     </React.Fragment>
