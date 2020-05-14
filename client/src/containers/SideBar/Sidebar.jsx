2411b9c273411c84b1a1f5969e06431dec1ce0c5
Carolina Martins <cnmss@iscte-iul.pt>
2020-05-14 12:58:28 +0100

    fixed sidebar bug

--- client/src/containers/SideBar/Sidebar.jsx
+++ client/src/containers/SideBar/Sidebar.jsx
@@ -201,7 +201,6 @@ class Sidebar extends Component {
               style={{
                 color: 'grey',
                 fontStyle: 'Italic',
-                position: 'fixed',
                 paddingLeft: '9%'
               }}
             >
