2ce8fc38cdab7cbe6abbbb7ae1afbe6f919a7941
lyudmylatodoriko <60884946+lyudmylatodoriko@users.noreply.github.com>
2020-06-12 15:10:54 +0100

    implemented sort on node logs, added generic sorting function (#141)

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -73,3 +73,19 @@ export default {
   ROLE_TYPE,
   VERSION
 };
+
+export const sortBy = (field, reverse, primer) => {
+  const key = primer
+    ? function(x) {
+        return primer(x[field]);
+      }
+    : function(x) {
+        return x[field];
+      };
+
+  reverse = !reverse ? 1 : -1;
+
+  return function(a, b) {
+    return (a = key(a)), (b = key(b)), reverse * ((a > b) - (b > a));
+  };
+};
