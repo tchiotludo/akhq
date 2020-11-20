985929bbb36b29545bb8bbf840be7d612f3f8bad
tchiotludo <tchiot.ludo@gmail.com>
2020-11-20 21:50:59 +0100

    feat(connect): add missing properties from connector
    close #521

--- src/main/java/org/akhq/models/ConnectPlugin.java
+++ src/main/java/org/akhq/models/ConnectPlugin.java
@@ -40,15 +40,45 @@ public class ConnectPlugin {
     public Stream<Definition> registryDefintion() {
         return Stream.of(
             Definition.builder()
+                .name("schema.registry.url")
+                .group("Schema Registry")
+                .displayName("Schema registry Url")
+                .importance("MEDIUM")
+                .build(),
+            Definition.builder()
+                .name("schema.registry.basic.auth.user.info")
+                .group("Schema Registry")
+                .displayName("Schema registry basic auth as user:password")
+                .importance("MEDIUM")
+                .build(),
+            Definition.builder()
+                .name("basic.auth.credentials.source")
+                .group("Schema Registry")
+                .displayName("Schema registry basic auth source")
+                .importance("MEDIUM")
+                .build(),
+            Definition.builder()
+                .name("key.converter.schemas.enable")
+                .group("Schema Registry")
+                .displayName("Enable key schemas")
+                .importance("MEDIUM")
+                .build(),
+            Definition.builder()
                 .name("key.converter.schema.registry.url")
                 .group("Schema Registry")
                 .displayName("Key schema registry Url")
                 .importance("MEDIUM")
                 .build(),
             Definition.builder()
-                .name("value.converter.schema.registry.url")
+                .name("key.converter.schema.registry.basic.auth.user.info")
                 .group("Schema Registry")
-                .displayName("Value schema registry Url")
+                .displayName("Key schema registry basic auth as user:password")
+                .importance("MEDIUM")
+                .build(),
+            Definition.builder()
+                .name("key.converter.basic.auth.credentials.source")
+                .group("Schema Registry")
+                .displayName("Key schema registry basic auth source")
                 .importance("MEDIUM")
                 .build(),
             Definition.builder()
@@ -58,9 +88,21 @@ public class ConnectPlugin {
                 .importance("MEDIUM")
                 .build(),
             Definition.builder()
-                .name("key.converter.schemas.enable")
+                .name("value.converter.schema.registry.url")
                 .group("Schema Registry")
-                .displayName("Enable key schemas")
+                .displayName("Value schema registry Url")
+                .importance("MEDIUM")
+                .build(),
+            Definition.builder()
+                .name("value.converter.schema.registry.basic.auth.user.info")
+                .group("Schema Registry")
+                .displayName("Value schema registry basic auth as user:password")
+                .importance("MEDIUM")
+                .build(),
+            Definition.builder()
+                .name("value.converter.basic.auth.credentials.source")
+                .group("Schema Registry")
+                .displayName("Value schema registry basic auth source")
                 .importance("MEDIUM")
                 .build()
         );
