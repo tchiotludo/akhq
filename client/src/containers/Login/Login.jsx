3691402b4c453df136f47e765bc8dbfc9b9a20db
Paulo <Marques>
2020-12-04 15:14:05 +0000

    #507

--- client/src/containers/Login/Login.jsx
+++ client/src/containers/Login/Login.jsx
@@ -68,21 +68,15 @@ class Login extends Form {
     }
   }
 
-  async componentDidMount() {
-    try {
-      const response = await this.getApi(uriAuths());
-      if (response.status === 200) {
-        const {loginEnabled, ...config} = response.data;
-        if (!loginEnabled) {
-          this.props.history.push({
-            pathname: '/ui',
-          });
-          return;
-        }
-        this.setState({config});
-      }
-    } catch (e) {
-      // cannot load auth config, use default
+  componentDidMount() {
+    const auths = JSON.parse(sessionStorage.getItem('auths'));
+    if(auths && auths.loginEnabled) {
+      const {loginEnabled, ...config} = auths;
+      this.setState({config});
+    } else {
+      this.props.history.push({
+        pathname: '/ui',
+      });
     }
   }
 
