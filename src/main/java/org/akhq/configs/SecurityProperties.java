7bd08d751081224f97802ad96117456e12fe21ee
Ronaldo Rother <ronaldo.rother@gmail.com>
2020-12-08 20:58:03 +0100

    fix(webserver): Rolling back group configuration to a map format (#526)

--- src/main/java/org/akhq/configs/SecurityProperties.java
+++ src/main/java/org/akhq/configs/SecurityProperties.java
@@ -1,15 +1,31 @@
 package org.akhq.configs;
 
+import com.google.common.base.Strings;
 import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.core.convert.format.MapFormat;
 import lombok.Data;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ConfigurationProperties("akhq.security")
 @Data
 public class SecurityProperties {
     private List<BasicAuth> basicAuth = new ArrayList<>();
-    private List<Group> groups;
     private String defaultGroup;
+
+    @MapFormat(transformation = MapFormat.MapTransformation.FLAT)
+    private Map<String, Group> groups = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        groups.forEach((key, group) -> {
+            if (Strings.isNullOrEmpty(group.getName())) {
+                group.setName(key);
+            }
+        });
+    }
 }
