31c7cf9085789fceb6657be2d453453a8eba1962
Pierre Larboulette <larboulette.pierre@gmail.com>
2021-03-09 22:20:43 +0100

    feat(connect): search on connect list (#602)

--- src/test/java/org/akhq/repositories/ConnectRepositoryTest.java
+++ src/test/java/org/akhq/repositories/ConnectRepositoryTest.java
@@ -11,6 +11,8 @@ import org.akhq.AbstractTest;
 import org.akhq.KafkaTestCluster;
 import org.akhq.models.ConnectDefinition;
 import org.akhq.models.ConnectPlugin;
+import org.akhq.utils.Pagination;
+import org.codehaus.httpcache4j.uri.URIBuilder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import scala.None;
 
 import javax.inject.Inject;
 import java.util.*;
@@ -98,10 +101,10 @@ public class ConnectRepositoryTest extends AbstractTest {
         );
 
 
-        List<ConnectDefinition> all1 = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1");
+        List<ConnectDefinition> all1 = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1", Optional.empty());
         assertEquals(1, all1.size());
 
-        List<ConnectDefinition> all2 = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-2");
+        List<ConnectDefinition> all2 = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-2", Optional.empty());
         assertEquals(1, all2.size());
 
         assertEquals(path1, repository.getDefinition(
@@ -152,8 +155,16 @@ public class ConnectRepositoryTest extends AbstractTest {
 
         repository.delete(KafkaTestCluster.CLUSTER_ID, "connect-1","ConnectRepositoryTest1");
         repository.delete(KafkaTestCluster.CLUSTER_ID, "connect-2","ConnectRepositoryTest2");
-        assertEquals(0, repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1").size());
-        assertEquals(0, repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-2").size());
+        assertEquals(0, repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1", Optional.empty()).size());
+        assertEquals(0, repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-2", Optional.empty()).size());
+    }
+
+    private void mockApplicationContext() {
+        Authentication auth = new DefaultAuthentication("test", Collections.singletonMap("connectsFilterRegexp", new ArrayList<>(Arrays.asList("^prefixed.*$"))));
+        DefaultSecurityService securityService = Mockito.mock(DefaultSecurityService.class);
+        when(securityService.getAuthentication()).thenReturn(Optional.of(auth));
+        when(applicationContext.containsBean(SecurityService.class)).thenReturn(true);
+        when(applicationContext.getBean(SecurityService.class)).thenReturn(securityService);
     }
 
     @Test
@@ -194,18 +205,50 @@ public class ConnectRepositoryTest extends AbstractTest {
         
         mockApplicationContext();
 
-        List<ConnectDefinition> filtered = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1");
+        List<ConnectDefinition> filtered = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1", Optional.empty());
         assertEquals(2, filtered.size());
         repository.delete(KafkaTestCluster.CLUSTER_ID, "connect-1","prefixed.Matching1");
         repository.delete(KafkaTestCluster.CLUSTER_ID, "connect-1","prefixed.Matching2");
         repository.delete(KafkaTestCluster.CLUSTER_ID, "connect-1","not.Matching3");
     }
-    private void mockApplicationContext() {
-        Authentication auth = new DefaultAuthentication("test", Collections.singletonMap("connectsFilterRegexp", new ArrayList<>(Arrays.asList("^prefixed.*$"))));
-        DefaultSecurityService securityService = Mockito.mock(DefaultSecurityService.class);
-        when(securityService.getAuthentication()).thenReturn(Optional.of(auth));
-        when(applicationContext.containsBean(SecurityService.class)).thenReturn(true);
-        when(applicationContext.getBean(SecurityService.class)).thenReturn(securityService);
+
+
+    @Test
+    public void getFilteredBySearchList() {
+
+        repository.create(
+                KafkaTestCluster.CLUSTER_ID,
+                "connect-1",
+                "prefixed.Matching1",
+                ImmutableMap.of(
+                        "connector.class", "FileStreamSinkConnector",
+                        "file", "/tmp/test.txt",
+                        "topics", KafkaTestCluster.TOPIC_CONNECT
+                )
+        );
+
+        repository.create(
+                KafkaTestCluster.CLUSTER_ID,
+                "connect-1",
+                "prefixed.Matching2",
+                ImmutableMap.of(
+                        "connector.class", "FileStreamSinkConnector",
+                        "file", "/tmp/test.txt",
+                        "topics", KafkaTestCluster.TOPIC_CONNECT
+                )
+        );
+
+        mockApplicationContext();
+
+        List<ConnectDefinition> notFiltered = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1", Optional.empty());
+        assertEquals(2, notFiltered.size());
+        List<ConnectDefinition> filtered = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1", Optional.of("prefixed.Matching1"));
+        assertEquals(1, filtered.size());
+        List<ConnectDefinition> filteredAll = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1", Optional.of("prefixed.Matching"));
+        assertEquals(2, filteredAll.size());
+
+        repository.delete(KafkaTestCluster.CLUSTER_ID, "connect-1","prefixed.Matching1");
+        repository.delete(KafkaTestCluster.CLUSTER_ID, "connect-1","prefixed.Matching2");
     }
 
 }
