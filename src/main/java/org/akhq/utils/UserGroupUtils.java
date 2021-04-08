d5c6272f310e6af68337af456ad97c4ef98f15c7
Julien Chanaud <chanaud.julien@gmail.com>
2021-04-08 22:25:20 +0200

    feat(auth): allows List or String in topics-filter-regex (#659)
    close #524
    close #656

--- src/main/java/org/akhq/utils/UserGroupUtils.java
+++ src/main/java/org/akhq/utils/UserGroupUtils.java
@@ -53,7 +53,7 @@ public class UserGroupUtils {
             .flatMap(group -> (group.getAttributes() != null) ? group.getAttributes().entrySet().stream() : null)
             .collect(Collectors.toMap(
                 Map.Entry::getKey,
-                item -> new ArrayList<>(Collections.singletonList(item.getValue())),
+                item -> new ArrayList<>(item.getValue()),
                 (e1, e2) -> {
                     ((List) e1).addAll((List) e2); return e1;
                 }
