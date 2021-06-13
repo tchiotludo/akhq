717ba81229583c8aa630baaa5d8ae6d6faefd8c7
tchiotludo <tchiot.ludo@gmail.com>
2021-06-13 20:52:21 +0200

    test(auths): add a test for basic auth case sensitive
    close #722

--- src/test/java/org/akhq/modules/BasicAuthAuthenticationProviderTest.java
+++ src/test/java/org/akhq/modules/BasicAuthAuthenticationProviderTest.java
@@ -1,8 +1,8 @@
 package org.akhq.modules;
 
 import io.micronaut.security.authentication.*;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.reactivex.Flowable;
-import org.akhq.AbstractTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
@@ -13,7 +13,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BasicAuthAuthenticationProviderTest extends AbstractTest {
+@MicronautTest
+public class BasicAuthAuthenticationProviderTest {
     @Inject
     BasicAuthAuthenticationProvider auth;
 
@@ -31,6 +32,30 @@ public class BasicAuthAuthenticationProviderTest extends AbstractTest {
 
         assertTrue(userDetail.isAuthenticated());
         assertEquals("user", userDetail.getUsername());
+
+        Collection<String> roles = userDetail.getRoles();
+
+        assertThat(roles, hasSize(4));
+        assertThat(roles, hasItem("topic/read"));
+        assertThat(roles, hasItem("registry/version/delete"));
+
+        assertEquals("test.*", ((List)userDetail.getAttributes("roles", "username").get("topicsFilterRegexp")).get(0));
+    }
+
+    @Test
+    public void successCase() {
+        AuthenticationResponse response = Flowable
+            .fromPublisher(auth.authenticate(null, new UsernamePasswordCredentials(
+                "MyUser3!@yàhöù.com",
+                "pass"
+            ))).blockingFirst();
+
+        assertThat(response, instanceOf(UserDetails.class));
+
+        UserDetails userDetail = (UserDetails) response;
+
+        assertTrue(userDetail.isAuthenticated());
+        assertEquals("MyUser3!@yàhöù.com", userDetail.getUsername());
 
         Collection<String> roles = userDetail.getRoles();
 
