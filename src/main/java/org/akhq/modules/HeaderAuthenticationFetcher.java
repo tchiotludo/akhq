8a1fa6cf440e582c9e5645a48768655664b6a79c
tchiotludo <tchiot.ludo@gmail.com>
2021-08-12 14:37:13 +0200

    feat(auth): add a header authentification for reverse proxy
    close #724

---
+++ src/main/java/org/akhq/modules/HeaderAuthenticationFetcher.java
@@ -0,0 +1,116 @@
+package org.akhq.modules;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.async.publisher.Publishers;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.security.authentication.Authentication;
+import io.micronaut.security.authentication.AuthenticationUserDetailsAdapter;
+import io.micronaut.security.authentication.Authenticator;
+import io.micronaut.security.authentication.UserDetails;
+import io.micronaut.security.filters.AuthenticationFetcher;
+import io.micronaut.security.token.config.TokenConfiguration;
+import io.reactivex.Flowable;
+import lombok.extern.slf4j.Slf4j;
+import org.akhq.configs.HeaderAuth;
+import org.akhq.utils.ClaimProvider;
+import org.reactivestreams.Publisher;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Requires(beans = HeaderAuth.class)
+@Singleton
+@Slf4j
+public class HeaderAuthenticationFetcher implements AuthenticationFetcher {
+    @Inject
+    HeaderAuth headerAuth;
+
+    @Inject
+    Authenticator authenticator;
+
+    @Inject
+    ClaimProvider claimProvider;
+
+    @Inject
+    TokenConfiguration configuration;
+
+    @Override
+    public Publisher<Authentication> fetchAuthentication(HttpRequest<?> request) {
+        Optional<String> userHeaders = request.getHeaders()
+            .get(headerAuth.getUserHeader(), String.class);
+
+        Optional<String> groupHeaders = headerAuth.getGroupsHeader() != null ?
+            request.getHeaders().get(headerAuth.getGroupsHeader(), String.class) :
+            Optional.empty();
+
+        if (userHeaders.isEmpty()) {
+            return Publishers.empty();
+        }
+
+        return Flowable
+            .fromCallable(() -> {
+                List<String> strings = groupsMapper(userHeaders.get(), groupHeaders);
+
+                if (strings.size() == 0) {
+                    return Optional.<ClaimProvider.AKHQClaimResponse>empty();
+                }
+
+                ClaimProvider.AKHQClaimRequest claim =
+                    ClaimProvider.AKHQClaimRequest.builder()
+                        .providerType(ClaimProvider.ProviderType.HEADER)
+                        .providerName(null)
+                        .username(userHeaders.get())
+                        .groups(strings)
+                        .build();
+
+                return Optional.of(claimProvider.generateClaim(claim));
+
+            })
+            .switchMap(t -> {
+                if (t.isPresent()) {
+                    UserDetails userDetails = new UserDetails(
+                        userHeaders.get(),
+                        t.get().getRoles(),
+                        t.get().getAttributes()
+                    );
+
+                    return Flowable.just(new AuthenticationUserDetailsAdapter(
+                        userDetails,
+                        configuration.getRolesName(),
+                        configuration.getNameKey()
+                    ));
+                } else {
+                    if (log.isDebugEnabled()) {
+                        log.debug("Could not authenticate {}", userHeaders.get());
+                    }
+                    return Flowable.empty();
+                }
+            });
+    }
+
+    private List<String> groupsMapper(String user, Optional<String> groupHeaders) {
+        if (headerAuth.getUsers() == null || headerAuth.getUsers().size() == 0) {
+            return groupsSplit(groupHeaders)
+                .collect(Collectors.toList());
+        }
+
+        return headerAuth
+            .getUsers()
+            .stream()
+            .filter(users -> users.getUsername().equals(user))
+            .flatMap(users -> Stream.concat(
+                groupsSplit(groupHeaders),
+                users.getGroups() != null ? users.getGroups().stream() : Stream.empty()
+            ))
+            .collect(Collectors.toList());
+    }
+
+    private Stream<String> groupsSplit(Optional<String> groupHeaders) {
+        return groupHeaders.stream().flatMap(s -> Arrays.stream(s.split(",")));
+    }
+}
---
+++ src/test/java/org/akhq/controllers/HeaderAuthControllerTest.java
@@ -0,0 +1,92 @@
+package org.akhq.controllers;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.akhq.AbstractTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HeaderAuthControllerTest extends AbstractTest {
+    @Inject
+    @Client("/")
+    protected RxHttpClient client;
+
+    @Test
+    void user() {
+        AkhqController.AuthUser result = client.toBlocking().retrieve(
+            HttpRequest
+                .GET("/api/me")
+                .header("x-akhq-user", "header-user"),
+            AkhqController.AuthUser.class
+        );
+
+        assertEquals("header-user", result.getUsername());
+        assertEquals(6, result.getRoles().size());
+    }
+
+    @Test
+    void admin() {
+        AkhqController.AuthUser result = client.toBlocking().retrieve(
+            HttpRequest
+                .GET("/api/me")
+                .header("x-akhq-user", "header-admin"),
+            AkhqController.AuthUser.class
+        );
+
+        assertEquals("header-admin", result.getUsername());
+        assertEquals(35, result.getRoles().size());
+    }
+
+    @Test
+    void userGroup() {
+        AkhqController.AuthUser result = client.toBlocking().retrieve(
+            HttpRequest
+                .GET("/api/me")
+                .header("x-akhq-user", "header-user")
+                .header("x-akhq-group", "limited,operator"),
+            AkhqController.AuthUser.class
+        );
+
+        assertEquals("header-user", result.getUsername());
+        assertEquals(11, result.getRoles().size());
+    }
+
+    @Test
+    void invalidUser() {
+        AkhqController.AuthUser result = client.toBlocking().retrieve(
+            HttpRequest
+                .GET("/api/me")
+                .header("x-akhq-user", "header-invalid"),
+            AkhqController.AuthUser.class
+        );
+
+        assertEquals(null, result.getUsername());
+        assertEquals(7, result.getRoles().size());
+    }
+
+    @MicronautTest(environments = "overridegroups")
+    public static class NoUser extends AbstractTest {
+        @Inject
+        @Client("/")
+        protected RxHttpClient client;
+
+        @Test
+        void invalidUser() {
+            AkhqController.AuthUser result = client.toBlocking().retrieve(
+                HttpRequest
+                    .GET("/api/me")
+                    .header("x-akhq-user", "header-user")
+                    .header("x-akhq-group", "limited,extra"),
+                AkhqController.AuthUser.class
+            );
+
+            assertEquals("header-user", result.getUsername());
+            assertEquals(3, result.getRoles().size());
+        }
+    }
+}
