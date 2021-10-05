3e5a4eccbf6a6c9733aa47d469bd1c44c5d9bb4e
bgranveaud <bgranveaud@infovista.com>
2021-10-05 09:54:54 +0200

    custom deserializer for avro raw format

--- src/main/java/org/akhq/repositories/CustomDeserializerRepository.java
+++ src/main/java/org/akhq/repositories/CustomDeserializerRepository.java
@@ -1,6 +1,8 @@
 package org.akhq.repositories;
 
 import org.akhq.modules.KafkaModule;
+import org.akhq.utils.AvroToJsonDeserializer;
+import org.akhq.utils.AvroToJsonSerializer;
 import org.akhq.utils.ProtobufToJsonDeserializer;
 
 import javax.inject.Inject;
@@ -12,15 +14,28 @@ import java.util.Map;
 public class CustomDeserializerRepository {
     @Inject
     private KafkaModule kafkaModule;
+    @Inject
+    private AvroToJsonSerializer avroToJsonSerializer;
     private final Map<String, ProtobufToJsonDeserializer> protobufToJsonDeserializers = new HashMap<>();
+    private final Map<String, AvroToJsonDeserializer> avroToJsonDeserializers = new HashMap<>();
 
     public ProtobufToJsonDeserializer getProtobufToJsonDeserializer(String clusterId) {
         if (!this.protobufToJsonDeserializers.containsKey(clusterId)) {
             this.protobufToJsonDeserializers.put(
                     clusterId,
-                    new ProtobufToJsonDeserializer(this.kafkaModule.getConnection(clusterId).getDeserialization())
+                    new ProtobufToJsonDeserializer(this.kafkaModule.getConnection(clusterId).getDeserialization().getProtobuf())
             );
         }
         return this.protobufToJsonDeserializers.get(clusterId);
+    }
+
+    public AvroToJsonDeserializer getAvroToJsonDeserializer(String clusterId) {
+        if (!this.avroToJsonDeserializers.containsKey(clusterId)) {
+            this.avroToJsonDeserializers.put(
+                clusterId,
+                new AvroToJsonDeserializer(this.kafkaModule.getConnection(clusterId).getDeserialization().getAvroRaw(), this.avroToJsonSerializer)
+            );
+        }
+        return this.avroToJsonDeserializers.get(clusterId);
     }
 }
