49b7c0f26d8ea69285de38dcb9a176ea1fc049ee
Julien Chanaud <chanaud.julien@gmail.com>
2021-12-20 21:39:27 +0100

    fix(auth): add missing test for RestApiClaimProvider (#959)
    Co-authored-by: Julien Chanaud <julien.chanaud@michelin.com>

---
+++ src/test/java/org/akhq/modules/RestApiClaimProviderTest.java
@@ -0,0 +1,96 @@
+package org.akhq.modules;
+
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTParser;
+import io.micronaut.context.annotation.Replaces;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MutableHttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Filter;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.client.DefaultHttpClientConfiguration;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.HttpClientConfiguration;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.filter.HttpServerFilter;
+import io.micronaut.http.filter.ServerFilterChain;
+import io.micronaut.rxjava2.http.client.RxHttpClient;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.AuthenticationRequest;
+import io.micronaut.security.authentication.UsernamePasswordCredentials;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.akhq.controllers.AkhqController;
+import org.akhq.middlewares.KafkaWrapperFilter;
+import org.akhq.utils.ClaimProvider;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import java.text.ParseException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(environments = "rest-api")
+public class RestApiClaimProviderTest {
+
+    @Inject
+    @Client("/")
+    protected RxHttpClient client;
+
+    @Test
+    void loginExternalClaim() throws ParseException {
+        HttpResponse<?> resultResponse = client.toBlocking().exchange(
+            HttpRequest.POST("/login", new UsernamePasswordCredentials("admin", "pass"))
+        );
+
+        assertTrue(resultResponse.getCookie("JWT").isPresent());
+
+        String jwtCookie = resultResponse.getCookie("JWT").get().getValue();
+        JWT token = JWTParser.parse(jwtCookie);
+
+        assertTrue(token.getJWTClaimsSet().getClaims().containsKey("topicsFilterRegexp"));
+        assertTrue(token.getJWTClaimsSet().getClaims().containsKey("roles"));
+
+        assertEquals("[\"filter1\",\"filter2\"]", token.getJWTClaimsSet().getClaims().get("topicsFilterRegexp").toString());
+        List<String> actualTopicFilters = token.getJWTClaimsSet().getStringListClaim("topicsFilterRegexp");
+        assertLinesMatch(List.of("filter1", "filter2"), actualTopicFilters);
+
+        List<String> actualRoles = token.getJWTClaimsSet().getStringListClaim("roles");
+        assertLinesMatch(List.of("topic/read", "group/read", "registry/read", "connect/read"), actualRoles);
+    }
+
+    @Requires(property = "akhq.security.rest.enabled", value = StringUtils.TRUE)
+    @Filter("/**")
+    @Replaces(KafkaWrapperFilter.class)
+    static class IgnoreKafkaWrapperFilter implements HttpServerFilter {
+
+        @Override
+        public Publisher<MutableHttpResponse<?>> doFilter(HttpRequest<?> request, ServerFilterChain chain) {
+            return chain.proceed(request);
+        }
+    }
+    @Requires(property = "akhq.security.rest.enabled", value = StringUtils.TRUE)
+    @PermitAll
+    @Controller("/external-mock")
+    static class RestApiExternalService {
+        @Post("/")
+        String generateClaim(ClaimProvider.AKHQClaimRequest request) {
+            return
+                "{\n" +
+                    "  \"roles\" : [ \"topic/read\", \"group/read\", \"registry/read\", \"connect/read\" ],\n" +
+                    "  \"attributes\" : {\n" +
+                    "    \"consumerGroupsFilterRegexp\" : [ \".*\" ],\n" +
+                    "    \"connectsFilterRegexp\" : [ \".*\" ],\n" +
+                    "    \"topicsFilterRegexp\" : [ \"filter1\", \"filter2\" ]\n" +
+                    "  }\n" +
+                    "}";
+        }
+    }
+}
+
