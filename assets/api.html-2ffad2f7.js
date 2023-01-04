cb9db64b658b446d886eda978e29c3cca9535aa0
tchiotludo <tchiotludo@users.noreply.github.com>
2023-01-04 09:02:29 +0000

    Automated deployment: Wed Jan  4 09:02:29 UTC 2023 c94c1e0ec20650b79df2c500b699128401de7ae2

---
+++ assets/api.html-2ffad2f7.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-d2e3fc0c","path":"/docs/api.html","title":"Api","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1672822900000,"contributors":[{"name":"lucapette","email":"ciao@lucapette.me","commits":1}]},"filePathRelative":"docs/api.md"}');export{e as data};
