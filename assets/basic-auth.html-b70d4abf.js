3a15aa405b2a53f3f8f515a4a12012bb5569e436
tchiotludo <tchiotludo@users.noreply.github.com>
2023-01-04 13:54:00 +0000

    Automated deployment: Wed Jan  4 13:54:00 UTC 2023 33d5c7a3f23f2350b9302a4f2c044bf19ac0ca60

--- assets/basic-auth.html-b70d4abf.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-6b377f67","path":"/docs/configuration/authentifications/basic-auth.html","title":"Basic Auth","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1672822900000,"contributors":[{"name":"lucapette","email":"ciao@lucapette.me","commits":1}]},"filePathRelative":"docs/configuration/authentifications/basic-auth.md"}');export{t as data};
