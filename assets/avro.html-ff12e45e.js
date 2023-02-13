8475128bd4104f78fa195d64635c74c8c866cef1
tchiotludo <tchiotludo@users.noreply.github.com>
2023-02-13 21:20:30 +0000

    Automated deployment: Mon Feb 13 21:20:30 UTC 2023 64179c2cb362f421f5f8fadb3cc1456778b17154

---
+++ assets/avro.html-ff12e45e.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-206cc69a","path":"/docs/configuration/avro.html","title":"Avro deserialization","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1676323171000,"contributors":[{"name":"Ludovic DEHON","email":"tchiot.ludo@gmail.com","commits":1}]},"filePathRelative":"docs/configuration/avro.md"}');export{t as data};
--- assets/groups.html-f3828834.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-c69887cc","path":"/docs/configuration/authentifications/groups.html","title":"Groups","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1676322703000,"contributors":[{"name":"Ludovic DEHON","email":"tchiot.ludo@gmail.com","commits":1}]},"filePathRelative":"docs/configuration/authentifications/groups.md"}');export{t as data};
