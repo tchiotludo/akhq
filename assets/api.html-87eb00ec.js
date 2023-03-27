1a2b3f1fc5ca0599970831ed2896199a6ee1cb5c
tchiotludo <tchiotludo@users.noreply.github.com>
2023-03-27 09:54:19 +0000

    Automated deployment: Mon Mar 27 09:54:19 UTC 2023 5d62e5ba75745532462e9680f3ddf694b5919611

--- assets/api.html-87eb00ec.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-d2e3fc0c","path":"/docs/api.html","title":"Api","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1679491884000,"contributors":[{"name":"Rochelle Gracias","email":"5615849+graciasrochelle@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/api.md"}');export{e as data};
--- assets/avro.html-f5e98864.js
+++
@@ -1,1 +0,0 @@
-const a=JSON.parse('{"key":"v-206cc69a","path":"/docs/configuration/avro.html","title":"Avro deserialization","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1679491884000,"contributors":[{"name":"Rochelle Gracias","email":"5615849+graciasrochelle@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/avro.md"}');export{a as data};
