8b1881b25480d963b1ed09506f15dee441a758bb
tchiotludo <tchiotludo@users.noreply.github.com>
2023-04-04 07:46:19 +0000

    Automated deployment: Tue Apr  4 07:46:19 UTC 2023 20fdc75c78322ed89d31a25e81944205c7e8fb15

---
+++ assets/index.html-88c437f7.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-b3a08698","path":"/docs/configuration/","title":"Configuration","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1680594324000,"contributors":[{"name":"Ludovic DEHON","email":"tchiot.ludo@gmail.com","commits":1}]},"filePathRelative":"docs/configuration/README.md"}');export{t as data};
