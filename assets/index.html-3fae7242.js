b77b2e330530ba8d62ef8651632e2ed05a0dd0c3
tchiotludo <tchiotludo@users.noreply.github.com>
2023-04-26 19:47:43 +0000

    Automated deployment: Wed Apr 26 19:47:43 UTC 2023 4713261b4191b49208f0fb522742e8b2f7f4064c

--- assets/index.html-3fae7242.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"Home","lang":"en-US","frontmatter":{"home":true,"title":"Home","sidebar":false,"pageClass":"landing"},"headers":[],"git":{"updatedTime":1681908262000,"contributors":[{"name":"Boris Momčilović","email":"boris@firstbeatmedia.com","commits":1}]},"filePathRelative":"README.md"}');export{e as data};
