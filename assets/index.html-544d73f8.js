e6d1cb2cc0d04c60f67ac65d8623584020e95332
tchiotludo <tchiotludo@users.noreply.github.com>
2023-05-05 14:25:37 +0000

    Automated deployment: Fri May  5 14:25:37 UTC 2023 2f4b020dcbb1cb367885c6b29baafb8ab5b44a62

---
+++ assets/index.html-544d73f8.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-b3a08698","path":"/docs/configuration/","title":"Configuration","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1683296683000,"contributors":[{"name":"jheinitz","email":"jheinitz@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/README.md"}');export{t as data};
