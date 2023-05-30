521a6cd3784041b0d8b512044aede3bfbd450c65
AlexisSouquiere <alexis.souquiere@gmail.com>
2023-05-30 09:13:30 +0200

    Fix unit tests

--- src/test/java/org/akhq/repositories/ConnectRepositoryTest.java
+++ src/test/java/org/akhq/repositories/ConnectRepositoryTest.java
@@ -22,7 +22,6 @@ import jakarta.inject.Inject;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 class ConnectRepositoryTest extends AbstractTest {
@@ -141,7 +140,7 @@ class ConnectRepositoryTest extends AbstractTest {
     }
 
     private void mockApplicationContext() {
-        Authentication auth = new ServerAuthentication("test", List.of(), Collections.singletonMap("connectsFilterRegexp", new ArrayList<>(Arrays.asList("^prefixed.*$"))));
+        Authentication auth = new ServerAuthentication("test", List.of(), Map.of());
         DefaultSecurityService securityService = Mockito.mock(DefaultSecurityService.class);
         when(securityService.getAuthentication()).thenReturn(Optional.of(auth));
         when(applicationContext.containsBean(SecurityService.class)).thenReturn(true);
@@ -186,7 +185,7 @@ class ConnectRepositoryTest extends AbstractTest {
 
         mockApplicationContext();
 
-        List<ConnectDefinition> filtered = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1", Optional.empty(), List.of());
+        List<ConnectDefinition> filtered = repository.getDefinitions(KafkaTestCluster.CLUSTER_ID, "connect-1", Optional.empty(), List.of("^prefixed.*$"));
         assertEquals(2, filtered.size());
         repository.delete(KafkaTestCluster.CLUSTER_ID, "connect-1", "prefixed.Matching1");
         repository.delete(KafkaTestCluster.CLUSTER_ID, "connect-1", "prefixed.Matching2");
