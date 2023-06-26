3981cc950e2ff30beb30479d589be356ff4c13d7
tchiotludo <tchiotludo@users.noreply.github.com>
2023-06-26 20:08:42 +0000

    Automated deployment: Mon Jun 26 20:08:42 UTC 2023 f241803670d85495fb961a86a145171cf2c4d9b4

---
+++ assets/avro.html-f1c1989b.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-206cc69a","path":"/docs/configuration/avro.html","title":"Avro deserialization","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1687810068000,"contributors":[{"name":"jheinitz","email":"jheinitz@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/avro.md"}');export{t as data};
