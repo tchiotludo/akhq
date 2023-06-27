dd8214a1d9dc6b5ae990a52395400449801627b8
tchiotludo <tchiotludo@users.noreply.github.com>
2023-06-27 06:07:35 +0000

    Automated deployment: Tue Jun 27 06:07:35 UTC 2023 a6f5cb4851c56c039488d5db52de8ebba5f25902

--- assets/api.html-1b456757.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-d2e3fc0c","path":"/docs/api.html","title":"Api","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1687845351000,"contributors":[{"name":"Markus","email":"3304672+markush81@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/api.md"}');export{e as data};
