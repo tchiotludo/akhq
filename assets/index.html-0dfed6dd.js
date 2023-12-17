0c1eed3a4d4691b7fc2a2c353638768fff7a8682
tchiotludo <tchiotludo@users.noreply.github.com>
2023-12-17 21:15:26 +0000

    Automated deployment: Sun Dec 17 21:15:26 UTC 2023 2e452666b012557ae6fe9df1170d703ab924acbc

--- assets/index.html-0dfed6dd.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-75b1ef1a","path":"/docs/configuration/schema-registry/","title":"Schema registry","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1702847538000,"contributors":[{"name":"Alexis SOUQUIERE","email":"alexis.souquiere@gmail.com","commits":1}]},"filePathRelative":"docs/configuration/schema-registry/README.md"}');export{e as data};
