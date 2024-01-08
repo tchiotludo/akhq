ba7a875ec670271e0ded716047cce3adcbcf913c
AlexisSouquiere <alexis.souquiere@gmail.com>
2024-01-08 20:32:18 +0100

    Fixing blocking call after Micronaut migration

--- src/main/java/org/akhq/security/claim/RestApiClaimProvider.java
+++ src/main/java/org/akhq/security/claim/RestApiClaimProvider.java
@@ -6,6 +6,8 @@ import io.micronaut.core.util.StringUtils;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 import org.akhq.models.security.ClaimProvider;
 import org.akhq.models.security.ClaimRequest;
 import org.akhq.models.security.ClaimResponse;
@@ -13,6 +15,7 @@ import org.akhq.models.security.ClaimResponse;
 @Primary
 @Requires(property = "akhq.security.rest.enabled", value = StringUtils.TRUE)
 @Client("${akhq.security.rest.url}")
+@ExecuteOn(TaskExecutors.BLOCKING)
 public interface RestApiClaimProvider extends ClaimProvider {
     @Post
     @Override
