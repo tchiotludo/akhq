70223a95c275f68110f9cf0145b5f06fc89ae68b
emmanuel <emmanuel.brard@getyourguide.com>
2024-02-23 15:43:17 +0100

    Remove annotation

--- src/main/java/org/akhq/configs/Audit.java
+++ src/main/java/org/akhq/configs/Audit.java
@@ -1,14 +1,11 @@
 package org.akhq.configs;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
-import io.micronaut.core.bind.annotation.Bindable;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 @ConfigurationProperties("akhq.audit")
 public class Audit {
-    @NotBlank
     Boolean enabled = false;
     String clusterId;
     String topicName;
