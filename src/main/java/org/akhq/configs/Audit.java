a399ead45de43abe204b8773ddb4be6f787b8328
emmanuel <emmanuel.brard@getyourguide.com>
2024-02-23 15:30:22 +0100

    Set audit off by default

--- src/main/java/org/akhq/configs/Audit.java
+++ src/main/java/org/akhq/configs/Audit.java
@@ -1,12 +1,15 @@
 package org.akhq.configs;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.core.bind.annotation.Bindable;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 @ConfigurationProperties("akhq.audit")
 public class Audit {
-    Boolean enabled;
+    @NotBlank
+    Boolean enabled = false;
     String clusterId;
     String topicName;
 }
