43dd0c4f49aacad29d2a194f854b65248bcea5c6
Ludovic DEHON <tchiot.ludo@gmail.com>
2024-04-04 15:27:55 +0200

    fix(topicdata): missing noargs for headers

--- src/main/java/org/akhq/models/KeyValue.java
+++ src/main/java/org/akhq/models/KeyValue.java
@@ -2,12 +2,14 @@ package org.akhq.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * Represents a simple key-value pair of any type
  */
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class KeyValue<K,V> {
     K key;
     V value;
