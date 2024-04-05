a51b759bf95b0cb4dbbb2c581ebee46e9f622337
tchiotludo <tchiotludo@users.noreply.github.com>
2024-04-05 12:08:13 +0000

    Automated deployment: Fri Apr  5 12:08:13 UTC 2024 1e770c0af752ef1e7732e87cd52db097d0ffc66a

---
+++ assets/api.html-k1uSKeh4.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-d2e3fc0c","path":"/docs/api.html","title":"Api","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1712318860000,"contributors":[{"name":"Jonas Voelcker","email":"47037735+jonasvoelcker@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/api.md"}');export{e as data};
