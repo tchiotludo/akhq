35cfd6952d60603106db17e0fa8b6499d29c0eec
emmanuel.brard <emmanuel.brard@getyourguide.com>
2024-04-05 09:44:38 +0200

    Fix make KafkaAuditModuleNoSecTest extend AbstractTest

--- src/test/java/org/akhq/modules/audit/KafkaAuditModuleNoSecTest.java
+++ src/test/java/org/akhq/modules/audit/KafkaAuditModuleNoSecTest.java
@@ -1,15 +1,18 @@
 package org.akhq.modules.audit;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micronaut.context.ApplicationContext;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import org.akhq.AbstractTest;
 import org.akhq.KafkaClusterExtension;
 import org.akhq.KafkaTestCluster;
+import org.akhq.modules.KafkaModule;
 import org.akhq.repositories.TopicRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
@@ -21,13 +24,14 @@ import java.util.concurrent.ExecutionException;
  */
 @ExtendWith(KafkaClusterExtension.class)
 @MicronautTest(environments = "audit-no-security")
-public class KafkaAuditModuleNoSecTest {
+public class KafkaAuditModuleNoSecTest extends AbstractTest  {
 
     @Inject
     @InjectMocks
     protected TopicRepository topicRepository;
 
-    private final ObjectMapper mapper = new ObjectMapper();
+    @Mock
+    ApplicationContext applicationContext;
 
     @BeforeEach
     void before() {
@@ -35,10 +39,9 @@ public class KafkaAuditModuleNoSecTest {
     }
 
     @Test
-    void topicAudit() throws ExecutionException, InterruptedException, IOException {
+    void topicAuditNoSec() throws ExecutionException, InterruptedException {
         topicRepository.create(KafkaTestCluster.CLUSTER_ID, "dummy-no-sec", 1, (short) 1, Collections.emptyList()
         );
-
     }
 
 }
