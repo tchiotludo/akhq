8d38f92fac0204a4a9cedb63eb6765faf0ae62f7
tchiotludo <tchiotludo@users.noreply.github.com>
2024-04-15 22:10:40 +0000

    deploy: ecd9a6c1e7f204b6858c8272dc0697ab811ec047

--- assets/glue.html-5PUHuxWC.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-1853d1ea","path":"/docs/configuration/schema-registry/glue.html","title":"Glue schema registry","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1713213076000,"contributors":[{"name":"Ludovic DEHON","email":"tchiot.ludo@gmail.com","commits":1}]},"filePathRelative":"docs/configuration/schema-registry/glue.md"}');export{e as data};
