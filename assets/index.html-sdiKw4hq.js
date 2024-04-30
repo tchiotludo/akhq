bddbf71739dcc1658d934776747a9883d1a19079
tchiotludo <tchiotludo@users.noreply.github.com>
2024-04-30 14:18:18 +0000

    deploy: 4e1122ca8a0a29a25b5ebbb8bf709abd7bd3d7c5

--- assets/index.html-sdiKw4hq.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"Home","lang":"en-US","frontmatter":{"home":true,"title":"Home","sidebar":false,"pageClass":"landing"},"headers":[],"git":{"updatedTime":1714039187000,"contributors":[{"name":"Jonas Voelcker","email":"47037735+jonasvoelcker@users.noreply.github.com","commits":1}]},"filePathRelative":"README.md"}');export{e as data};
