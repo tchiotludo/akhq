ff58014d9317cb7c9e7003a3d4c383ed106e12a4
Cenk Kılıç <c.kilic@rewe-group.at>
2024-05-02 15:11:11 +0200

    fix(auth): use client filter

--- src/main/java/org/akhq/security/claim/RestApiClaimProviderFilter.java
+++ src/main/java/org/akhq/security/claim/RestApiClaimProviderFilter.java
@@ -3,33 +3,25 @@ package org.akhq.security.claim;
 import java.util.List;
 import java.util.Map;
 
-import org.reactivestreams.Publisher;
-
-import edu.umd.cs.findbugs.annotations.Nullable;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.context.annotation.Value;
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MutableHttpRequest;
 import io.micronaut.http.annotation.ClientFilter;
-import io.micronaut.http.filter.ClientFilterChain;
-import io.micronaut.http.filter.HttpClientFilter;
+import io.micronaut.http.annotation.RequestFilter;
 
 @Requires(property = "akhq.security.rest.enabled", value = StringUtils.TRUE)
 @Requires(property = "akhq.security.rest.headers")
-@ClientFilter("${akhq.security.rest.url}/**")
-public class RestApiClaimProviderFilter implements HttpClientFilter {
+@ClientFilter("${akhq.security.rest.url}")
+public class RestApiClaimProviderFilter {
     private static final String HEADER_KEY = "name";
     private static final String HEADER_VALUE = "value";
 
-    @Nullable
     @Value("${akhq.security.rest.headers}")
     private List<Map<String, String>> headers;
 
-    @Override
-    public Publisher<? extends HttpResponse<?>> doFilter(MutableHttpRequest<?> request, ClientFilterChain chain) {
+    @RequestFilter
+    public void filter(MutableHttpRequest<?> request) {
         this.headers.forEach(header -> request.header(header.get(HEADER_KEY), header.get(HEADER_VALUE)));
-
-        return chain.proceed(request);
     }
 }
