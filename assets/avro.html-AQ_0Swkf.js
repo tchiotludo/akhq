727a6d9e29d1bf5732174478dd87e3623f9bbf35
tchiotludo <tchiotludo@users.noreply.github.com>
2024-05-15 21:27:22 +0000

    deploy: e5458e9fe0d034f5f579f4bceb5be901f4567908

--- assets/avro.html-AQ_0Swkf.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-206cc69a","path":"/docs/configuration/avro.html","title":"Avro deserialization","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1714394291000,"contributors":[{"name":"dependabot[bot]","email":"49699333+dependabot[bot]@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/avro.md"}');export{t as data};
