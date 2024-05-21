f9968fc178b80b8167a14067614add2f81abc666
Alexis SOUQUIERE <alexis.souquiere@gmail.com>
2024-05-21 09:01:19 +0200

    feat(auth): add default value for github api and update doc (#1779)
    Fix #1775

--- src/main/java/org/akhq/security/authentication/GithubApiClient.java
+++ src/main/java/org/akhq/security/authentication/GithubApiClient.java
@@ -8,7 +8,7 @@ import org.reactivestreams.Publisher;
 
 @Header(name = "User-Agent", value = "Micronaut")
 @Header(name = "Accept", value = "application/vnd.github.v3+json, application/json")
-@Client("${github.api.url}")
+@Client("${github.api.url:`https://api.github.com`}")
 public interface GithubApiClient {
 
     @Get("/user")
