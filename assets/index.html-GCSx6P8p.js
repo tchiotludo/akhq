f7524294503908ba0a03ac68e668138a6d0eac5d
tchiotludo <tchiotludo@users.noreply.github.com>
2024-05-27 20:36:18 +0000

    deploy: 21d360f6dc2020f3dc385fc826945982f19ae6af

--- assets/index.html-GCSx6P8p.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-b3a08698","path":"/docs/configuration/","title":"Configuration","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1716494660000,"contributors":[{"name":"Sverre H. Huseby","email":"shh+github@thathost.com","commits":1}]},"filePathRelative":"docs/configuration/README.md"}');export{t as data};
