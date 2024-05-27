b87b332dd10fb9c4db877aaaed3aa72cddce23b4
doxsch <28098153+doxsch@users.noreply.github.com>
2024-05-27 20:48:44 +0200

    fix: endpoint topic/{topicName}/groups incorrect acl resource type
    closes #1789

--- src/main/java/org/akhq/controllers/TopicController.java
+++ src/main/java/org/akhq/controllers/TopicController.java
@@ -272,7 +272,7 @@ public class TopicController extends AbstractController {
         return this.topicRepository.findByName(cluster, topicName).getPartitions();
     }
 
-    @AKHQSecured(resource = Role.Resource.TOPIC, action = Role.Action.READ)
+    @AKHQSecured(resource = Role.Resource.CONSUMER_GROUP, action = Role.Action.READ)
     @Get("api/{cluster}/topic/{topicName}/groups")
     @Operation(tags = {"topic"}, summary = "List all consumer groups from a topic")
     public List<ConsumerGroup> groups(String cluster, String topicName) throws ExecutionException, InterruptedException {
