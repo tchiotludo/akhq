3b1ba3f621af7376fa4a7e16bed8a7f8082435b9
Mohamed Hammad <m.hammad@rewe-group.at>
2024-06-03 16:24:22 +0200

    fix: changing the filter to implement HttpClient

--- src/main/java/org/akhq/security/claim/RestApiClaimProviderFilter.java
+++ src/main/java/org/akhq/security/claim/RestApiClaimProviderFilter.java
@@ -1,23 +1,27 @@
 package org.akhq.security.claim;
 
-import java.util.List;
-import java.util.Map;
-
 import io.micronaut.context.annotation.Primary;
 import io.micronaut.context.annotation.Property;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.util.StringUtils;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MutableHttpRequest;
 import io.micronaut.http.annotation.ClientFilter;
-import io.micronaut.http.annotation.RequestFilter;
+import io.micronaut.http.filter.ClientFilterChain;
+import io.micronaut.http.filter.HttpClientFilter;
+import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
 
 @Primary
 @Requires(property = "akhq.security.rest.enabled", value = StringUtils.TRUE)
 @Requires(property = "akhq.security.rest.headers")
 @ClientFilter("${akhq.security.rest.url}")
-public class RestApiClaimProviderFilter {
+public class RestApiClaimProviderFilter implements HttpClientFilter {
     private static final Logger LOG = LoggerFactory.getLogger(RestApiClaimProviderFilter.class);
 
     private static final String HEADER_KEY = "name";
@@ -26,10 +30,10 @@ public class RestApiClaimProviderFilter {
     @Property(name = "akhq.security.rest.headers")
     private List<Map<String, String>> headers;
 
-    @RequestFilter
-    public void filter(MutableHttpRequest<?> request) {
-        LOG.trace("Modifying outgoing authentication request.");
-
+    @Override
+    public Publisher<? extends HttpResponse<?>> doFilter(MutableHttpRequest<?> request, ClientFilterChain chain) {
+        LOG.trace("Modifying outgoing authentication request. from muttable");
         headers.forEach(header -> request.header(header.get(HEADER_KEY), header.get(HEADER_VALUE)));
+        return Mono.from(chain.proceed(request));
     }
 }
