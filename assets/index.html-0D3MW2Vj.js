699ee90c189648f6e09a470ffd39b71bd90475a5
tchiotludo <tchiotludo@users.noreply.github.com>
2024-06-05 08:19:00 +0000

    deploy: 7d4cc70532bfb2e7a9b9b42ed6ee90f441f787d7

---
+++ assets/index.html-0D3MW2Vj.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-b3a08698","path":"/docs/configuration/","title":"Configuration","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1717575503000,"contributors":[{"name":"Jonas Voelcker","email":"47037735+jonasvoelcker@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/README.md"}');export{e as data};
