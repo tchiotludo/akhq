d876654044609a532c4047e764d7d8a7703f6262
tchiotludo <tchiotludo@users.noreply.github.com>
2024-06-05 08:19:32 +0000

    deploy: 2507fa1877869af06656722f1499c09080fa9876

---
+++ assets/api.html-6Qkw7-E_.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-d2e3fc0c","path":"/docs/api.html","title":"Api","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1717575538000,"contributors":[{"name":"Alexis SOUQUIERE","email":"alexis.souquiere@gmail.com","commits":1}]},"filePathRelative":"docs/api.md"}');export{e as data};
--- assets/avro.html-QTPmZF3V.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-206cc69a","path":"/docs/configuration/avro.html","title":"Avro deserialization","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1717575503000,"contributors":[{"name":"Jonas Voelcker","email":"47037735+jonasvoelcker@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/avro.md"}');export{e as data};
