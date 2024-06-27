a7faec932573cf4cc51e4c27e6de31cb48eaea02
tchiotludo <tchiotludo@users.noreply.github.com>
2024-06-27 19:54:20 +0000

    deploy: 20fbea7a937e79cbbb308d721af278be723e2ec6

---
+++ assets/api.html-J2DwO-Ie.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-d2e3fc0c","path":"/docs/api.html","title":"Api","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1719231699000,"contributors":[{"name":"dependabot[bot]","email":"49699333+dependabot[bot]@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/api.md"}');export{e as data};
