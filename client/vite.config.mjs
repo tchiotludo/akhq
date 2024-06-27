d7dd14b8e9fd571c79f6f416e2b82176c7d87acb
Jonas Voelcker <barmer@jonas-voelcker.de>
2024-06-27 21:42:25 +0200

    fix(ui): calculating recordCount without undefined (#1838)
    fixes #1837

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     base: '/ui',
