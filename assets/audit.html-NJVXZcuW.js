aa4779f7d06ba63fb8c60bec14b1c5610ac2c071
tchiotludo <tchiotludo@users.noreply.github.com>
2024-06-27 20:04:45 +0000

    deploy: 0ad541cb7d9d6c1d348050741aff3c18541a9041

---
+++ assets/audit.html-NJVXZcuW.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-04a77cdc","path":"/docs/configuration/audit.html","title":"Audit configuration","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1719518374000,"contributors":[{"name":"Alexis SOUQUIERE","email":"alexis.souquiere@gmail.com","commits":1}]},"filePathRelative":"docs/configuration/audit.md"}');export{t as data};
