85dde571d7cb0af1e7d381211b6b131ce91dd341
tchiotludo <tchiotludo@users.noreply.github.com>
2024-06-27 19:56:10 +0000

    deploy: 82023801d4f39e999d67d42f3a5a9566ce76f31f

--- assets/api.html-aKMLVV9G.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-d2e3fc0c","path":"/docs/api.html","title":"Api","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1718021377000,"contributors":[{"name":"dependabot[bot]","email":"49699333+dependabot[bot]@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/api.md"}');export{e as data};
