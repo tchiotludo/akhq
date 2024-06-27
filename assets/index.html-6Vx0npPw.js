7f1aab32d2efc5d868ea2af8659674a7e8617447
tchiotludo <tchiotludo@users.noreply.github.com>
2024-06-27 20:01:55 +0000

    deploy: 0be719d9321361eef150565ec9f06654230b99bb

---
+++ assets/index.html-6Vx0npPw.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-75b1ef1a","path":"/docs/configuration/schema-registry/","title":"Schema registry","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1719518305000,"contributors":[{"name":"Shane Kennedy","email":"shane.kennedy19@gmail.com","commits":1}]},"filePathRelative":"docs/configuration/schema-registry/README.md"}');export{e as data};
---
+++ assets/index.html-Vk9aJxwq.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"Home","lang":"en-US","frontmatter":{"home":true,"title":"Home","sidebar":false,"pageClass":"landing"},"headers":[],"git":{"updatedTime":1719518305000,"contributors":[{"name":"Shane Kennedy","email":"shane.kennedy19@gmail.com","commits":1}]},"filePathRelative":"README.md"}');export{e as data};
