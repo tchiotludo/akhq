fa459a1ece69fa68957327d1a1aacf930e915c9d
tchiotludo <tchiotludo@users.noreply.github.com>
2024-06-27 20:05:23 +0000

    deploy: 818f1da5558684fe9c39b458b472231127b35d31

---
+++ assets/index.html-ldHnRYsx.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-75b1ef1a","path":"/docs/configuration/schema-registry/","title":"Schema registry","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1719518396000,"contributors":[{"name":"Alexis SOUQUIERE","email":"alexis.souquiere@gmail.com","commits":1}]},"filePathRelative":"docs/configuration/schema-registry/README.md"}');export{e as data};
