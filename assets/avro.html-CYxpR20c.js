7cf7e1482d8029cb65802d4db9a0512ba4982b28
tchiotludo <tchiotludo@users.noreply.github.com>
2024-08-30 22:18:36 +0000

    deploy: a8c2964f5625541c6cd6bf792f985b98d968c49f

---
+++ assets/avro.html-CYxpR20c.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-206cc69a","path":"/docs/configuration/avro.html","title":"Avro deserialization","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1721653525000,"contributors":[{"name":"dependabot[bot]","email":"49699333+dependabot[bot]@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/avro.md"}');export{t as data};
