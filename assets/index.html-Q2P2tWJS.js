91994cd8e9c08a464249eba93b35b125c3775348
tchiotludo <tchiotludo@users.noreply.github.com>
2024-08-30 22:25:36 +0000

    deploy: 2ea45f810741e01dfcf8189f2f043693b7fe9710

--- assets/index.html-Q2P2tWJS.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-b3a08698","path":"/docs/configuration/","title":"Configuration","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1725056264000,"contributors":[{"name":"dependabot[bot]","email":"49699333+dependabot[bot]@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/README.md"}');export{t as data};
