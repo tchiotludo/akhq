07ec03c4f51d30afbcf843bc5d0320eaa7e1b966
tchiotludo <tchiotludo@users.noreply.github.com>
2024-08-30 22:25:03 +0000

    deploy: ca7a0e28826234f38c418a1fef292793fac3758e

--- assets/audit.html-m7A5jJ88.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-04a77cdc","path":"/docs/configuration/audit.html","title":"Audit configuration","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1721653516000,"contributors":[{"name":"dependabot[bot]","email":"49699333+dependabot[bot]@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/audit.md"}');export{t as data};
