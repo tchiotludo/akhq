ebb24bb56f06fbd0132191f44e49a30420214a1e
tchiotludo <tchiotludo@users.noreply.github.com>
2024-10-01 16:40:20 +0000

    deploy: bccd6300b337921e1533d648d93fd6fe9a1a1edc

---
+++ assets/avro.html-4sE2RTJe.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-206cc69a","path":"/docs/configuration/avro.html","title":"Avro deserialization","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1727800611000,"contributors":[{"name":"thibthibus","email":"92714817+thibthibus@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/avro.md"}');export{t as data};
---
+++ assets/index.html-fX9Hu32b.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-b3a08698","path":"/docs/configuration/","title":"Configuration","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1727800611000,"contributors":[{"name":"thibthibus","email":"92714817+thibthibus@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/README.md"}');export{t as data};
