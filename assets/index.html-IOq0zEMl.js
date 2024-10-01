27e11069b5ae5172752b57a3767b11b2982e49a4
tchiotludo <tchiotludo@users.noreply.github.com>
2024-10-01 16:34:46 +0000

    deploy: 4b708611ea57bf5866d065976eb4e6f57ade5706

--- assets/index.html-IOq0zEMl.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-75b1ef1a","path":"/docs/configuration/schema-registry/","title":"Schema registry","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1727800428000,"contributors":[{"name":"alwibrm","email":"35731816+alwibrm@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/configuration/schema-registry/README.md"}');export{e as data};
