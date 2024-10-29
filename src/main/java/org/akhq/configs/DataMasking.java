c7a4bfa7abf739453dbb2c2bd9fff045e1de8976
James Forward <james@forwardeder.com>
2024-10-29 19:09:29 +0000

    fix(masking): Remove DataMaskingMode

--- src/main/java/org/akhq/configs/DataMasking.java
+++ src/main/java/org/akhq/configs/DataMasking.java
@@ -10,7 +10,6 @@ import java.util.List;
 @Data
 public class DataMasking {
    List<RegexFilter> filters = new ArrayList<>(); // regex filters still use `filters` for backwards compatibility
-   DataMaskingMode mode = DataMaskingMode.REGEX; // set this by default to REGEX for backwards compatibility for current regex filtering users who haven't defined this property.
    List<JsonMaskingFilter> jsonFilters = new ArrayList<>();
    String jsonMaskReplacement = "xxxx";
    boolean cachingEnabled = false;
--- src/test/java/org/akhq/utils/DefaultMaskerSettingTest.java
+++ src/test/java/org/akhq/utils/DefaultMaskerSettingTest.java
@@ -2,28 +2,18 @@ package org.akhq.utils;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.akhq.configs.DataMasking;
 import org.junit.jupiter.api.Test;
 
-import static org.akhq.configs.DataMaskingMode.REGEX;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 @MicronautTest
 public class DefaultMaskerSettingTest {
 
     @Inject
-    DataMasking dataMasking;
-
-    @Inject
     Masker masker;
 
     @Test
     void defaultValuesShouldUseRegexForBackwardsCompatibility() {
-        assertEquals(
-            REGEX,
-            dataMasking.getMode()
-        );
         assertInstanceOf(RegexMasker.class, masker);
     }
 }
