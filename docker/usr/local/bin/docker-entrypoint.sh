b4a64991ff6117e00d8d6c133cdf7ebdd8c7998b
Erik Godding Boye <egboye@gmail.com>
2024-12-18 23:15:29 +0100

    fix(docker): extend (not override) base image entrypoint

--- docker/usr/local/bin/docker-entrypoint.sh
+++ docker/usr/local/bin/docker-entrypoint.sh
@@ -6,4 +6,9 @@ if [ "${AKHQ_CONFIGURATION}" ]; then
     echo "${AKHQ_CONFIGURATION}" > /app/application.yml
 fi
 
-exec "$@"
+# We currently override the base image ENTRYPOINT in our Dockerfile making it harder
+# to get access to the CA certificates feature provided by the base image ENTRYPOINT:
+# https://github.com/docker-library/docs/blob/master/eclipse-temurin/content.md#can-i-add-my-internal-ca-certificates-to-the-truststore
+# To fix this issue, we invoke the base image ENTRYPOINT script (/__cacert_entrypoint.sh) before the arguments.
+# Credits to https://superuser.com/a/1460890
+exec /__cacert_entrypoint.sh "$@"
