7c452dc871da2ffacc346bba26523e85a3e7534f
AmitAnveri <anveri.a@northeastern.edu>
2025-03-27 10:45:08 -0400

    Removed log

--- client/src/containers/Topic/Topic/Topic.jsx
+++ client/src/containers/Topic/Topic/Topic.jsx
@@ -66,7 +66,6 @@ class Topic extends Root {
         topicInternal: this.props.location.internal
       },
       () => {
-        console.log('Topic data current:', this.topicData.current);
         this.getTopicsConfig();
         let uri = `/ui/${clusterId}/topic/${topicId}/${this.state.selectedTab}`;
         if (searchParams) {
