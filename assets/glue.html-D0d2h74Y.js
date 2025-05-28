0d93cceca998ac62896c960f35cd8697727de2d8
tchiotludo <tchiotludo@users.noreply.github.com>
2025-05-28 21:10:40 +0000

    deploy: b66234537b389441ec3e7e9ec150aa6aef3fdb6b

---
+++ assets/glue.html-D0d2h74Y.js
@@ -0,0 +1,22 @@
+import{_ as n,c as a,b as e,o as t}from"./app-zC0J_fJG.js";const p={};function l(o,s){return t(),a("div",null,s[0]||(s[0]=[e(`<h1 id="glue-schema-registry" tabindex="-1"><a class="header-anchor" href="#glue-schema-registry"><span>Glue schema registry</span></a></h1><p>Currently ,glue schema registry support is limited to only de-serialisation of avro/protobuf/json serialized messages. It can be configured as below.</p><div class="language-yaml line-numbers-mode" data-highlighter="prismjs" data-ext="yml"><pre><code class="language-yaml"><span class="line">  <span class="token key atrule">akhq</span><span class="token punctuation">:</span></span>
+<span class="line">    <span class="token key atrule">environment</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token key atrule">AKHQ_CONFIGURATION</span><span class="token punctuation">:</span> <span class="token punctuation">|</span><span class="token scalar string"></span>
+<span class="line">        akhq:</span>
+<span class="line">          connections:</span>
+<span class="line">            docker-kafka-server:</span>
+<span class="line">              properties:</span>
+<span class="line">                bootstrap.servers: &quot;kafka:9092&quot;</span>
+<span class="line">              schema-registry:</span>
+<span class="line">                url: &quot;http://schema-registry:8085&quot;</span>
+<span class="line">                type: &quot;glue&quot;</span>
+<span class="line">                glueSchemaRegistryName: Name of schema Registry</span>
+<span class="line">                awsRegion: aws region</span>
+<span class="line">              connect:</span>
+<span class="line">                - name: &quot;connect&quot;</span>
+<span class="line">                  url: &quot;http://connect:8083&quot;</span></span>
+<span class="line">    <span class="token key atrule">ports</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token punctuation">-</span> 8080<span class="token punctuation">:</span><span class="token number">8080</span></span>
+<span class="line">    <span class="token key atrule">links</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token punctuation">-</span> kafka</span>
+<span class="line">      <span class="token punctuation">-</span> repo</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"></div></div><p>Please note that authentication is done using aws default credentials provider.</p><p>Url key is required to not break the flow.</p>`,5)]))}const i=n(p,[["render",l]]),r=JSON.parse('{"path":"/docs/configuration/schema-registry/glue.html","title":"Glue schema registry","lang":"en-US","frontmatter":{},"git":{"updatedTime":1748466071000,"contributors":[{"name":"dependabot[bot]","username":"dependabot[bot]","email":"49699333+dependabot[bot]@users.noreply.github.com","commits":1,"url":"https://github.com/dependabot[bot]"}],"changelog":[{"hash":"b66234537b389441ec3e7e9ec150aa6aef3fdb6b","time":1748466071000,"email":"49699333+dependabot[bot]@users.noreply.github.com","author":"dependabot[bot]","message":"chore(deps-dev): bump @babel/core from 7.24.4 to 7.27.1 in /client"}]},"filePathRelative":"docs/configuration/schema-registry/glue.md"}');export{i as comp,r as data};
