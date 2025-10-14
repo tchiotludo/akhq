60000d78a8e8427492b80fbb65914857ce21a9a6
AlexisSouquiere <alexis.souquiere@michelin.com>
2025-10-14 12:25:02 +0200

    Remove inline script to fix CSP issue

--- client/src/prefix.jsx
+++ client/src/prefix.jsx
@@ -1,14 +1,21 @@
-export default () => {
-  // eslint-disable-next-line no-undef
-  let prefix = AKHQ_PREFIX_PATH;
+function extractPrefixFromUrl() {
+  // Extract prefix from current URL path
+  const path = window.location.pathname;
+  const uiIndex = path.indexOf('/ui');
 
-  // eslint-disable-next-line no-undef
-  if (prefix === undefined) {
-    prefix = '/';
+  if (uiIndex > 0) {
+    return path.substring(0, uiIndex);
   }
 
+  return '';
+}
+
+export default () => {
+  // Use environment variable or detect from window location
+  let prefix = extractPrefixFromUrl() || '/';
+
   if (prefix.endsWith('/')) {
-    prefix = prefix.substr(-1);
+    prefix = prefix.slice(0, -1);
   }
 
   return prefix;
