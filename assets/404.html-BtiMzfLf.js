993986f4730fbe5f8035e671a75e68427e1655e3
AlexisSouquiere <AlexisSouquiere@users.noreply.github.com>
2026-02-18 14:33:15 +0000

    deploy: 378562b01923f74e394d9aa942faf458ab5a9f4b

---
+++ assets/404.html-BtiMzfLf.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,d as n,o as a}from"./app-C5aP21rw.js";const l={};function r(s,t){return a(),o("div",null,[...t[0]||(t[0]=[n("p",null,"404 Not Found",-1)])])}const _=e(l,[["render",r]]),d=JSON.parse('{"path":"/404.html","title":"","lang":"en-US","frontmatter":{"layout":"NotFound"},"git":{},"filePathRelative":null}');export{_ as comp,d as data};
