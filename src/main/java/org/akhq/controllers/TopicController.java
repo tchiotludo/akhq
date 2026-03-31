9aec839f48a59fb45d0008af5f2f42347a435d10
Suis-yorushika <dar.rain0@gmail.com>
2026-03-31 05:21:19 -0400

    Fix #2601: allow cleanup.policy and retention to use cluster default by filtering empty configs

--- src/main/java/org/akhq/controllers/TopicController.java
+++ src/main/java/org/akhq/controllers/TopicController.java
@@ -149,6 +149,7 @@ public class TopicController extends AbstractController {
             (configs != null ? configs : ImmutableMap.<String, String>of())
                 .entrySet()
                 .stream()
+                .filter(r -> r.getValue()!=null&&!r.getValue().isEmpty())
                 .map(r -> new Config(r.getKey(), r.getValue()))
                 .collect(Collectors.toList())
         );
